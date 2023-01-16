05946955bc5df4bc54b04c23a00bb2a2e0975082
shivam kushwah <kushwah.shivam30@gmail.com>
2023-01-16 13:51:24 +0530

    issue resolved

--- inc/patterns/team.php
+++ inc/patterns/team.php
@@ -13,11 +13,11 @@ return array(
     'content'    => '<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"100px","right":"20px","bottom":"100px","left":"20px"},"margin":{"top":"0","bottom":"0"}},"border":{"bottom":{"color":"#eeeeee","width":"1px"}}},"backgroundColor":"bg-sec","layout":{"type":"constrained"}} -->
     <div class="wp-block-group has-bg-sec-background-color has-background" style="border-bottom-color:#eeeeee;border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:100px;padding-right:20px;padding-bottom:100px;padding-left:20px"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
     <div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"bottom":"10px","left":"0","right":"0"}}},"fontSize":"extra-large"} -->
-    <h2 class="has-text-align-center has-extra-large-font-size" style="margin-right:0;margin-bottom:10px;margin-left:0">Team</h2>
+    <h2 class="has-text-align-center has-extra-large-font-size" style="margin-right:0;margin-bottom:10px;margin-left:0">'.esc_html__('Team','blockline').'</h2>
     <!-- /wp:heading -->
     
     <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"right":"0","bottom":"10px","left":"0","top":"0"}}},"fontSize":"upper-heading"} -->
-    <p class="has-text-align-center has-upper-heading-font-size" style="margin-top:0;margin-right:0;margin-bottom:10px;margin-left:0">We have a powerful &amp; hardworking team which works for you</p>
+    <p class="has-text-align-center has-upper-heading-font-size" style="margin-top:0;margin-right:0;margin-bottom:10px;margin-left:0">'.esc_html__('We have a powerful &amp; hardworking team which works for you','blockline').'</p>
     <!-- /wp:paragraph -->
     
     <!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"0px"},"margin":{"top":"80px"}}}} -->
@@ -30,12 +30,12 @@ return array(
     <!-- /wp:group -->
     
     <!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"5px","right":"0","bottom":"5px","left":"0"}}},"fontSize":"upper-heading"} -->
-    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">Gabriel labreez</h4>
+    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">'.esc_html__('Gabriel labreez','blockline').'</h4>
     <!-- /wp:heading -->
     
     <!-- wp:group {"style":{"border":{"top":{"width":"0px","style":"none"},"right":{"color":"var:preset|color|primary","width":"2px"},"bottom":{"color":"var:preset|color|primary","width":"2px"},"left":{"color":"var:preset|color|primary","width":"2px"}},"spacing":{"padding":{"top":"0","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-color:var(--wp--preset--color--primary);border-right-width:2px;border-bottom-color:var(--wp--preset--color--primary);border-bottom-width:2px;border-left-color:var(--wp--preset--color--primary);border-left-width:2px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"color":{"text":"#8d8d8d"}}} -->
-    <p class="has-text-align-center has-text-color" style="color:#8d8d8d;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>Co Founder</strong></p>
+    <p class="has-text-align-center has-text-color" style="color:#8d8d8d;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>'.esc_html__('Co Founder','blockline').'</strong></p>
     <!-- /wp:paragraph -->
     
     <!-- wp:social-links {"iconColor":"primary","iconColorValue":"#1a4548","size":"has-small-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"}} -->
@@ -58,12 +58,12 @@ return array(
     <!-- /wp:group -->
     
     <!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"5px","right":"0","bottom":"5px","left":"0"}}},"fontSize":"upper-heading"} -->
-    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">David Rasop</h4>
+    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">'.esc_html__('David Rasop','blockline').'</h4>
     <!-- /wp:heading -->
     
     <!-- wp:group {"style":{"border":{"top":{"width":"0px","style":"none"},"right":{"color":"var:preset|color|primary","width":"2px"},"bottom":{"color":"var:preset|color|primary","width":"2px"},"left":{"color":"var:preset|color|primary","width":"2px"}},"spacing":{"padding":{"top":"0","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-color:var(--wp--preset--color--primary);border-right-width:2px;border-bottom-color:var(--wp--preset--color--primary);border-bottom-width:2px;border-left-color:var(--wp--preset--color--primary);border-left-width:2px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"color":{"text":"#8d8d8d"}}} -->
-    <p class="has-text-align-center has-text-color" style="color:#8d8d8d;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>Manager</strong></p>
+    <p class="has-text-align-center has-text-color" style="color:#8d8d8d;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>'.esc_html__('Manager','blockline').'</strong></p>
     <!-- /wp:paragraph -->
     
     <!-- wp:social-links {"iconColor":"primary","iconColorValue":"#1a4548","size":"has-small-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"}} -->
@@ -86,12 +86,12 @@ return array(
     <!-- /wp:group -->
     
     <!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"5px","right":"0","bottom":"5px","left":"0"}}},"fontSize":"upper-heading"} -->
-    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">Karl Blac</h4>
+    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">'.esc_html__('Karl Blac','blockline').'</h4>
     <!-- /wp:heading -->
     
     <!-- wp:group {"style":{"border":{"top":{"width":"0px","style":"none"},"right":{"color":"var:preset|color|primary","width":"2px"},"bottom":{"color":"var:preset|color|primary","width":"2px"},"left":{"color":"var:preset|color|primary","width":"2px"}},"spacing":{"padding":{"top":"0","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-color:var(--wp--preset--color--primary);border-right-width:2px;border-bottom-color:var(--wp--preset--color--primary);border-bottom-width:2px;border-left-color:var(--wp--preset--color--primary);border-left-width:2px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"color":{"text":"#8d8d8d"}},"fontSize":"small"} -->
-    <p class="has-text-align-center has-text-color has-small-font-size" style="color:#8d8d8d;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>Support Manager</strong></p>
+    <p class="has-text-align-center has-text-color has-small-font-size" style="color:#8d8d8d;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>'.esc_html__('Support Manager','blockline').'</strong></p>
     <!-- /wp:paragraph -->
     
     <!-- wp:social-links {"iconColor":"primary","iconColorValue":"#1a4548","size":"has-small-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"}} -->
@@ -114,12 +114,12 @@ return array(
     <!-- /wp:group -->
     
     <!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"5px","right":"0","bottom":"5px","left":"0"}}},"fontSize":"upper-heading"} -->
-    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">Marin Stan</h4>
+    <h4 class="has-text-align-center has-upper-heading-font-size" style="margin-top:5px;margin-right:0;margin-bottom:5px;margin-left:0">'.esc_html__('Marin Stan','blockline').'</h4>
     <!-- /wp:heading -->
     
     <!-- wp:group {"style":{"border":{"top":{"width":"0px","style":"none"},"right":{"color":"var:preset|color|primary","width":"2px"},"bottom":{"color":"var:preset|color|primary","width":"2px"},"left":{"color":"var:preset|color|primary","width":"2px"}},"spacing":{"padding":{"top":"0","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-color:var(--wp--preset--color--primary);border-right-width:2px;border-bottom-color:var(--wp--preset--color--primary);border-bottom-width:2px;border-left-color:var(--wp--preset--color--primary);border-left-width:2px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"color":{"text":"#8d8d8d"}},"fontSize":"small"} -->
-    <p class="has-text-align-center has-text-color has-small-font-size" style="color:#8d8d8d;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>Hr Manager</strong></p>
+    <p class="has-text-align-center has-text-color has-small-font-size" style="color:#8d8d8d;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>'.esc_html__('Hr Manager','blockline').'</strong></p>
     <!-- /wp:paragraph -->
     
     <!-- wp:social-links {"iconColor":"primary","iconColorValue":"#1a4548","size":"has-small-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"}} -->
